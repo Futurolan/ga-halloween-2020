7a4a3871ba254d50914981af9fb5a565f4d88a13
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-09-30 23:40:31 +0200

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -1,7 +1,10 @@
 const menu = [
   { type: 'config', id: 'news' },
-  { type: 'config', id: 'info' }
-//  { type: 'config', id: 'partners' }
+  { type: 'config', id: 'live' },
+  { type: 'config', id: 'tickets' },
+  { type: 'config', id: 'tournaments' },
+  { type: 'config', id: 'info' },
+  { type: 'config', id: 'partners' }
 ]
 
 module.exports = menu
