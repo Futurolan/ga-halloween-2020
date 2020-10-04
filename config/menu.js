3b61f9c64466f8f173e4cc74f378013ef515ad5f
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-10-04 09:58:05 +0200

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -4,6 +4,7 @@ const menu = [
   { type: 'config', id: 'tickets' },
   { type: 'config', id: 'tournaments' },
   { type: 'config', id: 'info' },
+  { type: 'page', title: 'Covid-19', color: '#832470', id: 200424, link: '/covid-19' },
   { type: 'config', id: 'partners' }
 ]
 
